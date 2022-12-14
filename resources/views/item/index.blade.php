00a776980273a652316952a64416f02c07d4f0b9
tomitama <mtomita224@gmail.com>
2022-12-14 10:44:40 +0900

    ソート機能

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -10,15 +10,16 @@
 <div class="row">
     <div class="col-12">
         <!----検索バーここから--->
-<form method="GET" class="item-form" action="{{ route('index') }}">
-    <input type="search" placeholder="検索" name="search" value="@if (isset($search)) {{ $search }} @endif">
-    <div class="button-search">
-        <button type="submit">検索</button>
-        <button>
-            <a href="{{ route('index') }}" class="text-black">クリア</a>
-        </button>
-    </div>
-</form>
+        <form method="GET" class="item-form" action="{{ route('index') }}">
+            <input type="search" placeholder="検索" name="search" value="@if (isset($search)) {{ $search }} @endif">
+            <div class="button-search">
+                <button type="submit">検索</button>
+                <button>
+                    <a href="{{ route('index') }}" class="text-black">クリア</a>
+                </button>
+            </div>
+        </form>
+        <!---検索バーここまで--->
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">商品一覧</h3>
@@ -30,36 +31,39 @@
                     </div>
                 </div>
             </div>
-            <div class="card-body table-responsive p-0">
-                <table class="table table-hover text-nowrap">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>商品名</th>
-                            <th>金額</th>
-                            <th>在庫数</th>
-                            <th>種別</th>
-                            <th>詳細</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($items as $item)
-                        <tr>
-                            <td>{{ $item->id }}</td>
-                            <td>{{ $item->name }}</td>
-                            <td>{{ $item->price }}</td>
-                            <td>{{ $item->quantity }}</td>
-                            <td>{{\App\Models\Item::TYPE_LIST[$item->type] }}</td>
-                            <td>{{ $item->detail }}</td>
-                            <td><a href="{{ url('/item_update/'.$item -> id) }}" class="button_edit">編集</a>
-                                <!---idを渡す処理が必要---->
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $items->links();}}
+            <div id="items">
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-hover text-nowrap">
+                        <thead>
+                            <tr>
+                                <th class="sort test-id" data-sort="id">ID</th>
+                                <th class="sort test-name" data-sort="name">商品名</th>
+                                <th class="sort test-price" data-sort="price">金額</th>
+                                <th class="sort test-quantity" data-sort="quantity">在庫数</th>
+                                <th class="sort test-type" data-sort="type">種別</th>
+                                <th class="test-edit">詳細</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody class="list">
+                            @foreach ($items as $item)
+                            <tr>
+                                <td class="id">{{ $item->id }}</td>
+                                <td class="name">{{ $item->name }}</td>
+                                <td class="price">{{ $item->price }}</td>
+                                <td class="quantity">{{ $item->quantity }}</td>
+                                <td class="type">{{\App\Models\Item::TYPE_LIST[$item->type] }}</td>
+                                <td class="detail">{{ $item->detail }}</td>
+                                <td><a href="{{ url('/item_update/'.$item -> id) }}" class="button_edit">編集</a>
+                                    <!---idを渡す処理が必要---->
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $items->links();}}
+                    
+                </div>
             </div>
         </div>
     </div>
@@ -70,4 +74,15 @@
 @stop
 
 @section('js')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js"></script>
+                    <script>
+                        let options = {
+                            valueNames: ['id', 'name', 'price', 'quantity', 'type', ]
+                        };
+                        let userList = new List('items', options);
+                        // 初期状態はidで昇順ソートする
+                        userList.sort('id', {
+                            order: 'asc'
+                        });
+                    </script>
 @stop
--- resources/views/item/item_update.blade.php
+++ resources/views/item/item_update.blade.php
@@ -3,76 +3,74 @@
 @section('title', '商品編集')
 
 @section('content_header')
-    <h1>商品編集</h1>
+<h1>商品編集</h1>
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-md-10">
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                       @foreach ($errors->all() as $error)
-                          <li>{{ $error }}</li>
-                       @endforeach
-                    </ul>
-                </div>
-            @endif
-
-            <div class="card card-primary">
-                <form method="POST" action="/item_update/{{$id}}">    
-                    <div class="card-header">
-                        <h3 class="card-title">ID【No.{{$id}}】商品編集</h3>
-                    </div>
-                    <div class="card-tools"></div>
-                    @csrf
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="name">商品名</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="商品名" value="{{$name}}">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="type">種別</label>
-                            <select class="form-control" id="type" name="type" placeholder="選択してください" value="{{$type}}">
-                                @foreach(\App\Models\Item::TYPE_LIST as $key => $value)
-                                <option value="{{$key}}">{{$value}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="price">金額</label>
-                            <input type="number" class="form-control" id="price" name="price" placeholder="金額" value="{{$price}}">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="quantity">在庫数</label>
-                            <input type="number" class="form-control" id="quantity" name="quantity" placeholder="在庫数" value="{{$quantity}}">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="detail">詳細</label>
-                            <input type="text" class="form-control" id="detail" name="detail" placeholder="詳細説明"value="{{$quantity}}">
-                        </div>
-                        <!--更新ボタン---->
-                        <div class="input-button">
-                        <input type="submit" value="更新" class="btn btn-primary">
-</form>
-                        <!-- 削除ボタン -->
-                        <form action="/item_delete/{{$id}}" method="POST">
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger">削除</button>
-                            
-                        </form>
-                        </div>
-
-                    </div>
-            </div>
-                
-            </div>
+<div class="row">
+    <div class="col-md-10">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
+        @endif
+
+        <div class="card card-primary">
+            <form method="POST" action="/item_update/{{$id}}">
+                <div class="card-header">
+                    <h3 class="card-title">ID【No.{{$id}}】商品編集</h3>
+                </div>
+                <div class="card-tools"></div>
+                @csrf
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="name">商品名</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="商品名" value="{{$name}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="type">種別</label>
+                        <select class="form-control" id="type" name="type" placeholder="選択してください" value="{{$type}}">
+                            @foreach(\App\Models\Item::TYPE_LIST as $key => $value)
+                            <option value="{{$key}}">{{$value}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="price">金額</label>
+                        <input type="number" class="form-control" id="price" name="price" placeholder="金額" value="{{$price}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="quantity">在庫数</label>
+                        <input type="number" class="form-control" id="quantity" name="quantity" placeholder="在庫数" value="{{$quantity}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="detail">詳細</label>
+                        <input type="text" class="form-control" id="detail" name="detail" placeholder="詳細説明" value="{{$quantity}}">
+                    </div>
+                    <!--更新ボタン---->
+                    <div class="input-button">
+                        <input type="submit" value="更新" class="btn btn-primary">
+            </form>
+            <!-- 削除ボタン -->
+            <form action="/item_delete/{{$id}}" method="POST">
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-danger" onclick='return confirm("一度削除してしまうと戻せませんが、本当に削除してよいですか？");'>削除</button>
+            </form>
+
+        </div>
+
     </div>
+</div>
+
+
 @stop
 
 @section('css')
