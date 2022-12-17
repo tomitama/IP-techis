297ae9a3f22974698fda3cd8eae4a9c9a592047f
tomitama <mtomita224@gmail.com>
2022-12-17 13:27:02 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
+
         Paginator::useBootstrap();
         Schema::defaultStringLength(191);
         
