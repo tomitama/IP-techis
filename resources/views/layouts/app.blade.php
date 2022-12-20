a859d0548d15480cd6f00ea7ca17ce26ce448871
tomitama <mtomita224@gmail.com>
2022-12-20 17:22:31 +0900

    コンフリクト修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,6 @@
                                 </form>
                             </div>
                         </li>
-                        
                         @endguest
                     </ul>
                 </div>
