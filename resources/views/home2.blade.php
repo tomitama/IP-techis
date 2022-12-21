6a0ceb6b7cd173aa0483aa6c0f61032cb99102b1
tomitama <mtomita224@gmail.com>
2022-12-21 19:53:25 +0900

    ホーム画面修正

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -1,10 +1,10 @@
 @extends('adminlte::page')
 @section('title', 'Dashboard')
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>ホーム画面</h1>
 @stop
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>こちらはホーム画面です。</p>
 @stop
 @section('css')
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
